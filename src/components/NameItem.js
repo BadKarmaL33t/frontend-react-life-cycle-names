3a2389db3907f41c296926f462db27b7c6f56d91
BadKarmaL33t <jchagesteijn@protonmail.com>
2023-07-19 14:17:23 +0200

    opdracht uitwerken

--- src/components/NameItem.js
+++ src/components/NameItem.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
-function NameItem({ name }) {
+function NameItem({ name, activeName, setActiveName }) {
+
+    useEffect((e) => {
+        console.log(`${name} is in de DOM gezet`);
+        }, [name])
+
+    useEffect(() => {
+        if (activeName === name) {
+            console.log(`${name} wil graag een biertje bestellen`)
+        }
+    }, [activeName, name])
+
   return (
     <li>
       <h3>{name}</h3>
-      <button type="button">
+      <button
+          type="button"
+          onClick={() => setActiveName(name)}
+      >
         Geef een rondje!
       </button>
     </li>
+
   );
 }
 
